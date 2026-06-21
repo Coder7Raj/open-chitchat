f74ad40a304daa38750dbd6de9db8f9a83e73b91
Coder7Raj <alwaysraj733@gmail.com>
2026-06-22 05:36:47 +0600

    database connected successfully

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,4 +1,7 @@
+dotenv.config();
+import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./lib/db.js";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -6,6 +9,8 @@ const app = express();
 app.get("/api/auth/signup", (req, res) => {
   res.send("signup is working");
 });
+
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
+  connectDB();
 });
