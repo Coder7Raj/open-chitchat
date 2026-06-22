4cb8d939d7123701da8467f8b8958b4e95186834
Coder7Raj <alwaysraj733@gmail.com>
2026-06-22 07:06:25 +0600

    update some functionality & error handeling

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -31,14 +31,17 @@ export const signup = async (req, res) => {
 
     const newUser = new User({ username, email, password: passwordHash });
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
-      // res.status(201).json({ message: "User created successfully" });
+      //   generateToken(newUser._id, res);
+      //   await newUser.save();
+      //   res.status(201).json({ message: "User created successfully" });
+
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
       res.status(201).json({
-        _id: newUser._id,
-        username: newUser.username,
-        email: newUser.email,
-        profilePic: newUser.profilePic,
+        _id: savedUser._id,
+        username: savedUser.username,
+        email: savedUser.email,
+        profilePic: savedUser.profilePic,
       });
     } else {
       return res.status(400).json({ message: "Invalid user data" });
