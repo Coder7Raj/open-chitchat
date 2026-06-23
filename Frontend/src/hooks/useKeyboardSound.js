32c856d99b4ee74b93bdbfd817a2f042f4071222
Coder7Raj <alwaysraj733@gmail.com>
2026-06-24 00:12:55 +0600

    added message input fields and sound hook for typing sound in the input field

---
+++ Frontend/src/hooks/useKeyboardSound.js
@@ -0,0 +1,20 @@
+const keyStrokeSounds = [
+  new Audio("/sounds/keystroke1.mp3"),
+  new Audio("/sounds/keystroke2.mp3"),
+  new Audio("/sounds/keystroke3.mp3"),
+  new Audio("/sounds/keystroke4.mp3"),
+];
+
+export default function useKeyboardSound() {
+  const playRandomKeyStrokeSound = () => {
+    const randomSound =
+      keyStrokeSounds[Math.floor(Math.random() * keyStrokeSounds.length)];
+
+    randomSound.currentTime = 0;
+    randomSound
+      .play()
+      .catch((error) => console.log("audio play failed", error));
+  };
+
+  return { playRandomKeyStrokeSound };
+}
