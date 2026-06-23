91519143627493c90dc34f741aa757f8d535938e
Coder7Raj <alwaysraj733@gmail.com>
2026-06-23 23:48:51 +0600

    update chat store and added chat components

--- Frontend/src/pages/ChatPage.jsx
+++ Frontend/src/pages/ChatPage.jsx
@@ -1,12 +1,26 @@
+import BorderAnimationContainer from "../components/BorderAnimationContainer.jsx";
 import { useAuthStore } from "../store/useAuthStore.js";
 
 export default function ChatPage() {
   const { logout } = useAuthStore();
   return (
-    <div className="z-10">
-      chat page
-      <br />
-      <button onClick={logout}>Logout</button>
+    <div className="relative w-full max-w-6xl h-[800px]">
+      <BorderAnimationContainer>
+        {/* LEFT SIDE */}
+        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+          </div>
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+          {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+        </div>
+      </BorderAnimationContainer>
     </div>
   );
 }
