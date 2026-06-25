c772225922c63a60a0f71dceb18bce3a4b908b4d
Coder7Raj <alwaysraj733@gmail.com>
2026-06-25 22:41:11 +0600

    firebase setup successfully done

--- Frontend/src/components/NoChatHistoryPlaceholder.jsx
+++ Frontend/src/components/NoChatHistoryPlaceholder.jsx
@@ -7,7 +7,7 @@ export default function NoChatHistoryPlaceholder() {
         <MessageCircleIcon className="size-8 text-cyan-400" />
       </div>
       <h3 className="text-lg font-medium text-slate-200 mb-3">
-        Start your conversation with {name}
+        Start conversation with your friend
       </h3>
       <div className="flex flex-col space-y-3 max-w-md mb-5">
         <p className="text-slate-400 text-sm">
