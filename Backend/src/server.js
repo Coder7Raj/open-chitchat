f64a695959aa2a7f3e4a4247aa6836aca48902a0
Coder7Raj <alwaysraj733@gmail.com>
2026-07-31 23:00:31 +0600

    update root files

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,7 +7,6 @@ import express from "express";
 
 import connectDB from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
